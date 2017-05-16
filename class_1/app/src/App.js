079397ccef7ae79fc7a7d5891e64a601672b1990
Erwin Bader <erwin.bader06@gmail.com>
2017-05-16 18:15:29 -0300

    first commit

---
+++ class_1/app/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hola from './components/Hola';
+
+ReactDOM.render(<Hola />, document.getElementById("root"))
---
+++ class_1/app/src/components/hola.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Hola extends React.Component {
+	
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+		return (
+			<h1>Hola Mundo Erwin</h1>
+		)
+	}
+
+}
